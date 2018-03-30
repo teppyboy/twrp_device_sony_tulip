87423122d90fa7090eba03ddab3d6d38d971725b
Daniel Vasquez <danielgusvt@yahoo.com>
2018-03-29 20:40:24 -0500

    8.1 sources bringup
    Squashed from the older branches
    Fstab kanged from riseos

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,14 +3,14 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT := cortex-a53
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := MSM8916
@@ -21,12 +21,21 @@ TARGET_BOARD_PLATFORM := msm8916
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno405
 
 # Kernel
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/sony/tulip/dt.img
-BOARD_CUSTOM_BOOTIMG_MK := device/sony/tulip/mkbootimg.mk
+BUILD_KERNEL := true
+TARGET_KERNEL_CONFIG := tulip-mini_defconfig
+BOARD_KERNEL_BASE	    := 0x80000000
+BOARD_KERNEL_PAGESIZE	  := 2048
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
+
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=tulip msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
+
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
@@ -34,17 +43,17 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2675703808
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 4399808512
 TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
+# TARGET_USERIMAGES_USE_F2FS := true
 
 # Recovery
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_RECOVERY_SWIPE := true
-TARGET_PREBUILT_KERNEL := device/sony/tulip/kernel
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
 
 # TWRP Build Flags
-TARGET_RECOVERY_FSTAB := device/sony/tulip/recovery/etc/twrp.fstab
+TARGET_RECOVERY_FSTAB := device/sony/tulip/twrp.fstab
 TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 TW_INCLUDE_CRYPTO := true
--- omni_tulip.mk
+++ omni_tulip.mk
@@ -24,15 +24,9 @@ LOCAL_PATH := device/sony/tulip
 # Charger
 PRODUCT_PACKAGES += charger charger_res_images
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/dt.img:dt.img \
-    $(LOCAL_PATH)/kernel:kernel \
-    $(LOCAL_PATH)/fstab.qcom:root/fstab.qcom \
-    $(LOCAL_PATH)/recovery/etc/twrp.fstab:root/etc/twrp.fstab
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tulip
 PRODUCT_NAME := omni_tulip
 PRODUCT_BRAND := Sony
-PRODUCT_MODEL := Xperia M4 Aqua (AOSP)
+PRODUCT_MODEL := Xperia M4 Aqua
 PRODUCT_MANUFACTURER := Sony
